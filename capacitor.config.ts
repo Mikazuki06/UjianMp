8c8c7d8c9e38c15a50963e507d521d3e949245eb
Mikazuki06 <ikbalramdani061@gmail.com>
2023-06-10 16:13:42 +0700

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'UJI',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
